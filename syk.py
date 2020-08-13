c87a8c74bfbfc3b97e22d5cf14d8f122176862e5
Vincent Su <vipasu@berkeley.edu>
2020-08-13 10:53:36 -0700

    Adding documentation

--- syk.py
+++ syk.py
@@ -1,12 +1,21 @@
-from openfermion.ops import FermionOperator, MajoranaOperator
-from openfermion.transforms import bravyi_kitaev, bravyi_kitaev_fast, get_sparse_operator, jordan_wigner
-import numpy as np
-from itertools import combinations
-import openfermion as of
-import sympy
-import cirq
+"""
+syk.py implements the sampling of SYK model parameters and generates a Hamiltonian
+corresponding to $H_{\text{TFD}}$. After converting to Pauli strings, the
+Hamiltonian is saved to an output file. For small enough system sizes, we also compute
+the ground state energy via exact diagonalization.
+"""
 import argparse
+from itertools import combinations
+
+import numpy as np
+import sympy
 from scipy.linalg import eigh
+
+import cirq
+import openfermion as of
+from openfermion.ops import FermionOperator, MajoranaOperator
+from openfermion.transforms import (bravyi_kitaev, bravyi_kitaev_fast,
+                                    get_sparse_operator, jordan_wigner)
 
 
 def factorial(n):
@@ -32,10 +41,12 @@ def convert_H_majorana_to_qubit(inds, J_dict, N):
 
 
 def q_helper(idx):
+    """Returns qubit object based on index"""
     return cirq.LineQubit(idx)
 
 
 def construct_pauli_string(ham, key):
+    """Converts Pauli terms in the Hamiltonian to a string representation"""
     gate_dict = {'X': cirq.X,
                 'Y': cirq.Y,
                 'Z': cirq.Z}
@@ -48,11 +59,12 @@ def construct_pauli_string(ham, key):
 
 
 def sum_ops(operators):
-    # Wrapper for summing a list of majorana operators
+    """Wrapper for summing a list of majorana operators"""
     return sum(operators, MajoranaOperator((), 0))
 
 
 def gs_energy(hamiltonian):
+    """Use scipy to get the ground state energy"""
     from scipy.linalg import eigvalsh
     return eigvalsh(hamiltonian, eigvals=(0,0))
 
@@ -65,13 +77,16 @@ def main(N, seed, mu):
 
     J_var = 2**(q-1) * J**2 * factorial(q-1) / (q * N**(q-1))
 
-    # DONE: Figure out how to do the double copying
+    # Double copying by splitting L/R into two blocks
+    # Alternatively, could stagger the indices so they are paired differently
+    # into complex fermions
     L_indices = range(N)
     R_indices = range(N, 2*N)
     SYK_L_indices = list(combinations(L_indices, q))
     SYK_R_indices = list(combinations(R_indices, q))
     interaction_indices = [(l, r) for l, r in zip(L_indices, R_indices)]
-    # DONE: Generate couplings
+
+    # Generate couplings
     J_L, J_R = get_couplings(N, J_var, SYK_L_indices, SYK_R_indices, seed, q)
     interaction_strength = {ind: 1j * mu for ind in interaction_indices}
 
@@ -84,11 +99,13 @@ def main(N, seed, mu):
     matrix_ham = get_sparse_operator(total_ham) # todense() allows for ED
 
 
-    all_strings = list(total_ham.terms)
-    test_string = construct_pauli_string(total_ham, all_strings[0])
+    # Useful for debugging purposes
+    # all_strings = list(total_ham.terms)
+    # test_string = construct_pauli_string(total_ham, all_strings[0])
 
     # Diagonalize qubit hamiltonian to compare the spectrum of variational energy
     e, v = eigh(matrix_ham.todense())
+    # Only get the lowest 4 eigenvalues
     n_spec = 4
 
 
@@ -107,10 +124,10 @@ def main(N, seed, mu):
         for k, v in annihilation_ham.terms.items():
             f.write("{} => {}\n".format(np.real(v), k))
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-N", type=int, default=8, help="Number of fermions")
-    parser.add_argument("--mu", type=float, default=.01, help="Interaction strength")
     parser.add_argument("--seed", dest="seed", default=0, type=int, help="Random seed")
     parser.add_argument("--mu", dest="mu", default=0.01, type=float, help="Interaction mu")
     args = parser.parse_args()
