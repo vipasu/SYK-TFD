0e5bf94f9a3128fbbdd289f1e9091ddaf3baf65b
Vincent Su <vipasu@berkeley.edu>
2020-09-08 10:02:31 -0700

    Use Adam optimizer instead of SGD

--- read_ham.jl
+++ read_ham.jl
@@ -2,6 +2,7 @@ module read_ham
     using Yao, YaoExtensions
     using Plots
     using Printf
+    using Flux: Optimise
     export extract_pauli_term, construct_hamiltonian_from_terms, gradient_schedule, read_hamiltonian, read_annihilation, test_annihilation
     export default_train
 
@@ -20,36 +21,32 @@ module read_ham
         ham = map(lines) do line
             coef, pauli_terms = extract_pauli_term(line)
             pauli_string =  (map(pauli_terms) do item
-                            q_ind, pauli_choice = item
-                            q_ind = q_ind + 1 # conversion from python
-                            if pauli_choice == 'X'
-                            return sx(q_ind)
-                            elseif pauli_choice == 'Y'
-                            return sy(q_ind)
-                            elseif pauli_choice == 'Z'
-                            return sz(q_ind)
-                            else
-                            return "whips"
-                            end
+                             q_ind, pauli_choice = item
+                             q_ind = q_ind + 1 # conversion from python
+                             if pauli_choice == 'X'
+                                return sx(q_ind)
+                             elseif pauli_choice == 'Y'
+                                return sy(q_ind)
+                             elseif pauli_choice == 'Z'
+                                return sz(q_ind)
+                             else
+                                return "whips"
+                             end
                             end |> prod)
             coef * pauli_string
         end |> sum
     end
 
 
-    function gradient_schedule(num_iters, eta_1, eta_0)
-        map(x -> eta_1/x + eta_0, 1:num_iters)
-    end
-
-
-
     function read_hamiltonian(fname, N)
         lines = readlines(fname)
         SYK_energies = map(x -> parse(Float64, x), split(lines[1],","))
+        # N interaction terms appear at the end
+        # The remaining valid lines are split evenly between L/R
         left_lines = lines[2:Int((end-N+1)//2)]
         right_lines = lines[Int((end-N+3)//2):end-N]
         int_lines = lines[end-N+1:end]
-        # print(lines[1])
+
         h_L = construct_hamiltonian_from_terms(left_lines, N)
         h_R = construct_hamiltonian_from_terms(right_lines, N)
         h_int = construct_hamiltonian_from_terms(int_lines, N)
@@ -69,21 +66,29 @@ module read_ham
     end
 
 
-    function default_train(N, seed, mu=.01, d=50, num_steps=500)
+    function default_train(N, seed, mu=.01, d=50, num_steps=500; ent=cnot, nshots=nothing)
         ham_fname= "data/SYK_ham_$(N)_$(seed)_$(@sprintf("%.2f", mu)).txt"
-        ham, SYK_energies = read_hamiltonian(ham_fname, N)
+        h_syk, h_int, SYK_energies = read_hamiltonian(ham_fname, N)
+        h = h_syk + h_int
 
-        circuit = dispatch!(variational_circuit(N, d), :zero)
-        h = ham
+        circuit = dispatch!(variational_circuit(N, d, pair_ring(N), entangler=ent), :zero)
+        params = parameters(circuit)
+
+        opt = Optimise.Optimiser(Optimise.InvDecay(.03), Optimise.ADAM(.15))
 
         energies = []
         annihilators = []
-        etas = gradient_schedule(num_steps, .4, .04)
         energy = (real.(expect(h, uniform_state(N)=>circuit)))
         push!(energies, energy)
         for i in 1:num_steps
-            _, grad = expect'(h, uniform_state(N) => circuit)
-            dispatch!(-, circuit, etas[i]* grad)
+            if nshots === nothing
+                _, grad = expect'(h, uniform_state(N) => circuit)
+            else
+                grad = faithful_grad(h, uniform_state(N) => circuit; nshots=nshots)
+            end
+            Optimise.update!(opt, params, grad)
+
+            dispatch!(circuit, params)
             energy = (real.(expect(h, uniform_state(N)=>circuit)))
             parity = test_annihilation(N, seed, circuit, mu)
             if i % 10 == 0
